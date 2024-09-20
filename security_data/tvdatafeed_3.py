2fab7e82ed376c58e6f5e83eb2ac8e113be148a3
= <=>
2024-09-20 16:03:37 +0530

    Dependencies added

--- security_data/tvdatafeed_3.py
+++ security_data/tvdatafeed_3.py
@@ -9,24 +9,31 @@ def get_sec_data(sec_name,bars):
     return tv.get_hist(symbol= sec_name, exchange='NSE', interval=Interval.in_daily, n_bars=bars)
 
 
-df_data = pd.read_csv("C:/Users/Public/automation/genai/csvops/inputfiles/names3.csv")
+
+input_file_name = "C:/Users/Public/automation/genai/security_data/inputfiles/mid_rec.csv"
+output_file_name = "C:/Users/Public/automation/genai/security_data/outputfiles/mid_rec_processed.csv"
+df_data = pd.read_csv(input_file_name)
+
 
 name_list = []
 rec_date_list = []
 buy_price_list = []
 five_per_up_price_days_list = []
+five_per_up_price_count = 0
 ten_per_up_price_days_list = []
-rec_date_index = 0
+ten_per_up_price_count = 0
+df_data_row_index = 0
+
 for name in df_data.get("REC_SEC"):
     name_list.append(name)
     print("Security Name ******************************************* = " + name)
-    rec_date = df_data.get("REC_DT")[rec_date_index]
+    rec_date = df_data.get("REC_DT")[df_data_row_index]
     rec_date_list.append(rec_date)
     buy_price = ""
     five_per_up_price_days = ""
     ten_per_up_price_days = ""
 
-    rec_date = datetime.strptime(df_data.get("REC_DT")[rec_date_index], '%Y-%m-%d').date()
+    rec_date = datetime.strptime(df_data.get("REC_DT")[df_data_row_index], '%Y-%m-%d').date()
     today_date = datetime.today().strftime("%Y-%m-%d")
     today_date = datetime.strptime(today_date, '%Y-%m-%d').date()
     days_elapsed = (today_date - rec_date).days
@@ -59,6 +66,7 @@ for name in df_data.get("REC_SEC"):
                     five_per_up_price_days = (range_date-rec_date).days
                     five_per_up_price_days_list.append(five_per_up_price_days)
                     print("Closing price for 5per up on " + str(range_date) + " was " + str(close_price))
+                    five_per_up_price_count = five_per_up_price_count + 1
 
             ten_per_up_price = 1.10 * float(buy_price)
             if ten_per_up_price_days == "":
@@ -66,6 +74,7 @@ for name in df_data.get("REC_SEC"):
                     ten_per_up_price_days = (range_date - rec_date).days
                     ten_per_up_price_days_list.append(ten_per_up_price_days)
                     print("Closing price for 10 per up on " + str(range_date) + " was " + str(close_price))
+                    ten_per_up_price_count = ten_per_up_price_count +1
 
     if five_per_up_price_days=="":
         five_per_up_price_days_list.append("N/A")
@@ -85,9 +94,11 @@ for name in df_data.get("REC_SEC"):
                            "5PCR_UP_DATE": five_per_up_price_days_list,
                            "10PCR_UP_DATE": ten_per_up_price_days_list
                            }, columns=["REC_SEC","REC_DATE","BUY_PRICE","5PCR_UP_DATE","10PCR_UP_DATE"])
-    df_all.to_csv("price.csv", index=False)
+    df_all.to_csv(output_file_name, index=False)
 
-    rec_date_index = rec_date_index + 1
-    if rec_date_index == 100:
+    df_data_row_index = df_data_row_index + 1
+    if df_data_row_index == 100:
         exit()
-
+print("Number of securities that are in action  = "  + str(len(df_data)))
+print("Number of securities that are up by 5%  = "  + str(five_per_up_price_count))
+print("Number of securities that are up by 10%  = "  + str(ten_per_up_price_count))
