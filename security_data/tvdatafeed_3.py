4a7752f6fbdfe426800e8300b5881d834c0b950f
= <=>
2024-09-20 13:32:17 +0530

    Dependencies added

--- security_data/tvdatafeed_3.py
+++ security_data/tvdatafeed_3.py
@@ -14,10 +14,16 @@ df_data = pd.read_csv("C:/Users/Public/automation/genai/csvops/inputfiles/names3
 name_list = []
 rec_date_list = []
 buy_price_list = []
+five_per_up_price_days_list = []
+ten_per_up_price_days_list = []
+rec_date_index = 0
 for name in df_data.get("REC_SEC"):
     name_list.append(name)
-    rec_date_list.append(df_data.get("REC_DT")[0])
+    rec_date = df_data.get("REC_DT")[rec_date_index]
+    rec_date_list.append(rec_date)
     buy_price = ""
+    five_per_up_price_days = ""
+    ten_per_up_price_days = ""
 
     rec_date = datetime.strptime(df_data.get("REC_DT")[0], '%Y-%m-%d').date()
     today_date = datetime.today().strftime("%Y-%m-%d")
@@ -33,8 +39,6 @@ for name in df_data.get("REC_SEC"):
     for i in range(0,len(tv_data)):
         range_date = (tv_data.index[i]).date()
 
-
-
         if (range_date-rec_date).days >0:
             print("TV Date = " + str((tv_data.index[i]).date()))
             open_price = tv_data["open"].iloc[i]
@@ -42,48 +46,41 @@ for name in df_data.get("REC_SEC"):
 
             if buy_price == "":
                 buy_price = (open_price + close_price) / 2
+                buy_price = format(buy_price,".2f")
                 buy_price_list.append(buy_price)
 
 
+            five_per_up_price = 1.05 * float(buy_price)
+            if five_per_up_price_days =="":
+                if close_price > five_per_up_price:
+                    five_per_up_price_days = (range_date-rec_date).days
+                    five_per_up_price_days_list.append(five_per_up_price_days)
 
-print(len(name_list))
-print(len(rec_date_list))
-print(len(buy_price_list))
-df_all = pd.DataFrame({"REC_SEC": name_list,"REC_DATE": rec_date_list, "BUY_PRICE": buy_price_list} )
-df_all.to_csv("price.csv", index=False)
+            ten_per_up_price = 1.10 * float(buy_price)
+            if ten_per_up_price_days == "":
+                if close_price > ten_per_up_price:
+                    ten_per_up_price_days = (range_date - rec_date).days
+                    ten_per_up_price_days_list.append(ten_per_up_price_days)
 
-
-
-
-
-
-
-    # tv_df = get_sec_data(name)
-    # tv_df.to_csv("values.csv")
-    # tv_df = tv_df.sort_index(ascending=False)
-    # sec_datatime = tv_df.index[0]
-    # print(sec_datatime.date())
-    # print(tv_df["close"].iloc[0])
+    if five_per_up_price_days=="":
+        five_per_up_price_days_list.append("N/A")
+    if ten_per_up_price_days=="":
+        ten_per_up_price_days_list.append("N/A")
 
 
 
 
+    # print(len(name_list))
+    # print(len(rec_date_list))
+    # print(len(buy_price_list))
+    # print(len(five_per_up_price_days_list))
+    # print(len(ten_per_up_price_days_list))
 
+    df_all = pd.DataFrame({"REC_SEC": name_list,"REC_DATE": rec_date_list, "BUY_PRICE": buy_price_list,
+                           "5PCR_UP_DATE": five_per_up_price_days_list,
+                           "10PCR_UP_DATE": ten_per_up_price_days_list
+                           } )
+    df_all.to_csv("price.csv", index=False)
 
+    rec_date_index = rec_date_index + 1
 
-
-
-
-
-
-
-# tv = TvDatafeed()
-# stock_data  = tv.get_hist(symbol='TATAMOTORS',exchange='NSE',interval=Interval.in_daily,n_bars=5)
-# print(stock_data)
-# for day_index in range(len(stock_data)-1):
-#     print(stock_data["symbol"].iloc[day_index])
-#     print(stock_data["open"].iloc[day_index])
-#     print(stock_data["low"].iloc[day_index])
-#     print(stock_data["high"].iloc[day_index])
-#     print(stock_data["close"].iloc[day_index])
-#     print(stock_data["volume"].iloc[day_index])
