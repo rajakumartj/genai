4e0f5650ad4b23e159cab4d1dd84807c152b373d
= <=>
2024-09-20 14:45:56 +0530

    Dependencies added

--- security_data/tvdatafeed_3.py
+++ security_data/tvdatafeed_3.py
@@ -19,19 +19,19 @@ ten_per_up_price_days_list = []
 rec_date_index = 0
 for name in df_data.get("REC_SEC"):
     name_list.append(name)
+    print("Security Name ******************************************* = " + name)
     rec_date = df_data.get("REC_DT")[rec_date_index]
     rec_date_list.append(rec_date)
     buy_price = ""
     five_per_up_price_days = ""
     ten_per_up_price_days = ""
 
-    rec_date = datetime.strptime(df_data.get("REC_DT")[0], '%Y-%m-%d').date()
+    rec_date = datetime.strptime(df_data.get("REC_DT")[rec_date_index], '%Y-%m-%d').date()
     today_date = datetime.today().strftime("%Y-%m-%d")
     today_date = datetime.strptime(today_date, '%Y-%m-%d').date()
     days_elapsed = (today_date - rec_date).days
     print(days_elapsed)
 
-    print(type(rec_date))
     tv_data = get_sec_data(name,days_elapsed)
     print(len(tv_data))
 
@@ -44,6 +44,9 @@ for name in df_data.get("REC_SEC"):
             open_price = tv_data["open"].iloc[i]
             close_price = tv_data["close"].iloc[i]
 
+            print("Open Price = " + str(open_price))
+            print("Close price = " + str(close_price))
+
             if buy_price == "":
                 buy_price = (open_price + close_price) / 2
                 buy_price = format(buy_price,".2f")
@@ -55,12 +58,14 @@ for name in df_data.get("REC_SEC"):
                 if close_price > five_per_up_price:
                     five_per_up_price_days = (range_date-rec_date).days
                     five_per_up_price_days_list.append(five_per_up_price_days)
+                    print("Closing price for 5per up on " + str(range_date) + " was " + str(close_price))
 
             ten_per_up_price = 1.10 * float(buy_price)
             if ten_per_up_price_days == "":
                 if close_price > ten_per_up_price:
                     ten_per_up_price_days = (range_date - rec_date).days
                     ten_per_up_price_days_list.append(ten_per_up_price_days)
+                    print("Closing price for 10 per up on " + str(range_date) + " was " + str(close_price))
 
     if five_per_up_price_days=="":
         five_per_up_price_days_list.append("N/A")
@@ -79,8 +84,10 @@ for name in df_data.get("REC_SEC"):
     df_all = pd.DataFrame({"REC_SEC": name_list,"REC_DATE": rec_date_list, "BUY_PRICE": buy_price_list,
                            "5PCR_UP_DATE": five_per_up_price_days_list,
                            "10PCR_UP_DATE": ten_per_up_price_days_list
-                           } )
+                           }, columns=["REC_SEC","REC_DATE","BUY_PRICE","5PCR_UP_DATE","10PCR_UP_DATE"])
     df_all.to_csv("price.csv", index=False)
 
     rec_date_index = rec_date_index + 1
+    if rec_date_index == 100:
+        exit()
 
